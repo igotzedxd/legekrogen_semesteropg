6ce8b779056ced9781a06b73308ffbb3ca1ac6d5
igotzedxd <74432210+igotzedxd@users.noreply.github.com>
2024-08-09 12:40:43 +0200

    done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,32 @@
-import { useState, useEffect } from 'react';
-import styles from './footer.module.css';
+import { FaF, FaP } from "react-icons/fa6";
+import styles from "./footer.module.css";
+import { FaPhone, FaEnvelope, FaFacebook, FaInstagram } from "react-icons/fa";
 
-function footer() {
-  const [state, setState] = useState(null);
-
-  useEffect(() => {
-    // Your effect logic here
-  }, []);
-
+function Footer() {
   return (
     <div className={styles.container}>
-      footer
+      <div className={styles.inner}>
+        <div className={styles.contact}>
+          <h3 className="cursive">Kundeservice</h3>
+          <span>
+            <FaEnvelope />
+            <a href="#">kontakt@legehjørnet.dk</a>
+          </span>
+          <span>
+            <FaPhone />
+            <p>+45 23 45 67 89</p>
+          </span>
+        </div>
+        <div className={styles.socials}>
+          <h3 className="cursive">Følg os</h3>
+          <span>
+            <FaFacebook />
+            <FaInstagram />
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default footer;
+export default Footer;
