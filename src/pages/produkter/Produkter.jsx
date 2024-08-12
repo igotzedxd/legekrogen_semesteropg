823ad2f75989c9670bb6fdbe804ddbd9170fb362
igotzedxd <74432210+igotzedxd@users.noreply.github.com>
2024-08-12 11:28:39 +0200

    xd

--- src/pages/produkter/Produkter.jsx
+++ src/pages/produkter/Produkter.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import styles from "./products.module.css";
+import styles from "./produkter.module.css";
 import DeliveryMessage from "../../components/deliveryMessage/DeliveryMessage";
 import Hero from "../../components/hero/Hero";
 import Footer from "../../components/footer/Footer";
