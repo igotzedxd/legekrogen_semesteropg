59c350043846f0f366d7f8a7969ce24af2f5b218
igotzedxd <74432210+igotzedxd@users.noreply.github.com>
2024-08-13 16:41:56 +0200

    done

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -1,18 +1,56 @@
-import { useState, useEffect } from 'react';
-import styles from './faq.module.css';
+import { useState } from "react";
+import styles from "./faq.module.css";
+import useFetch from "../hooks/useFetch";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import parse from "html-react-parser";
 
-function faq() {
-  const [state, setState] = useState(null);
+function Faq() {
+  const { data: questions, error } = useFetch("https://legekrogen.webmcdm.dk/questions");
+  const [openIds, setOpenIds] = useState([]);
 
-  useEffect(() => {
-    // Your effect logic here
-  }, []);
+  const toggleOpenId = (id) => {
+    if (openIds.includes(id)) {
+      setOpenIds(openIds.filter((openId) => openId !== id));
+    } else {
+      setOpenIds([...openIds, id]);
+    }
+  };
+
+  const addDashesToText = (htmlContent) => {
+    // Replace <p> and <li> tags with a dash followed by the original content
+    const modifiedContent = htmlContent.replace(/<p>/g, "<p> ").replace(/<li>/g, "<li>- ");
+
+    return parse(modifiedContent);
+  };
+
+  if (error) {
+    return <p className={styles.error}>Error: {error.message}</p>;
+  }
 
   return (
     <div className={styles.container}>
-      faq
+      {questions ? (
+        questions.map((q) => (
+          <div key={q._id} onClick={() => toggleOpenId(q._id)} className={styles.faq}>
+            {/* render question */}
+            <p className={styles.q}>{q.question}</p>
+            {/* line to seperate Q and A */}
+            {openIds.includes(q._id) && <p className={styles.line}></p>}
+            {/* instead of styling display none, we just don't render it unless the container is clicked */}
+            {openIds.includes(q._id) && <div className={styles.a}>{addDashesToText(q.answer)}</div>}
+            {/* show up or down arrow depending if the container is clicked */}
+            {openIds.includes(q._id) ? (
+              <IoIosArrowUp className={styles.arrow} />
+            ) : (
+              <IoIosArrowDown className={styles.arrow} />
+            )}
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
 
-export default faq;
+export default Faq;
